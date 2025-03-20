28bf436eafeace01af292403764a4664973336e8
maddyyang <131908833+maddyyang@users.noreply.github.com>
2025-03-20 19:03:38 +0000

    jefftxt

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={labImage} className="logo react" alt="Custom logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Jeff</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
