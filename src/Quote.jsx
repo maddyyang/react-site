74ed3e01f1323359d8521911d0d247aceee3e156
maddyyang <131908833+maddyyang@users.noreply.github.com>
2025-03-30 18:12:34 +0000

    quotejsx

---
+++ src/Quote.jsx
@@ -0,0 +1,9 @@
+function Quote() {
+    return (
+      <blockquote style={{ fontStyle: 'italic', marginTop: '1rem' }}>
+        "Code is like humor. When you have to explain it, it’s bad."
+      </blockquote>
+    )
+  }
+  
+  export default Quote
